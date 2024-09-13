806252939ac138bc11f04edc16ec88eb7919ac50
relliv <epsilonelittr@gmail.com>
2024-09-14 00:23:26 +0300

    Add tailwind support to react app

--- apps/react-app/tailwind.config.js
+++ apps/react-app/tailwind.config.js
@@ -1,14 +1,9 @@
-const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
-const { join } = require('path');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{tsx,ts,js,jsx}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
