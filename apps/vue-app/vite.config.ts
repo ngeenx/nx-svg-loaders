9fed3b5530dce43805a1f3cfd0b2f9b083bfd074
relliv <epsilonelittr@gmail.com>
2024-09-13 06:43:07 +0300

    Add vue app

---
+++ apps/vue-app/vite.config.ts
@@ -0,0 +1,49 @@
+/// <reference types='vitest' />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: "../../node_modules/.vite/apps/vue-app",
+
+  server: {
+    port: 3100,
+    host: "localhost",
+  },
+
+  preview: {
+    port: 4300,
+    host: "localhost",
+  },
+
+  plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: "../../dist/apps/vue-app",
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+
+  test: {
+    watch: false,
+    globals: true,
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+
+    reporters: ["default"],
+    coverage: {
+      reportsDirectory: "../../coverage/apps/vue-app",
+      provider: "v8",
+    },
+  },
+});
