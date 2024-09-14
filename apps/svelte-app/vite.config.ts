bc3a3c94608d3597a2b9b9c6e031e3b637da9017
relliv <epsilonelittr@gmail.com>
2024-09-14 23:24:40 +0300

    Fix ESM conflict

--- apps/svelte-app/vite.config.ts
+++ apps/svelte-app/vite.config.ts
@@ -1,13 +1,8 @@
-// Add this to your imports
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    //... other plugins
-    svelte(), // Add this line
-  ],
-  //...
+  plugins: [svelte()],
   server: {
     port: 3200,
     host: "localhost",
