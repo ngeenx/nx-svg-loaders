0d83af11562ae448cdf1cfd1c2d35e31aec6ff58
relliv <epsilonelittr@gmail.com>
2024-09-14 03:20:00 +0300

    Add audio loader component

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,24 +1,156 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <main>
     <header>Vue SVG Loaders</header>
 
-    <section>
-      <h1>asdasdsa</h1>
-
+    <section
+      v-for="(loaderType, loaderIndex) in loaderTypes"
+      :key="loaderIndex"
+      class="flex flex-row items-center justify-center gap-5 p-4"
+    >
+      <h1>{{ loaderType }} Loaders</h1>
       <div>
-        <p>*</p>
-        <p>*</p>
-        <p>*</p>
-        <p>*</p>
-        <p>*</p>
+        <NxAudioLoader
+          v-if="loaderType === 'Audio'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <!-- <NxBallTriangleLoader
+          v-if="loaderType === 'Ball Triangle'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxBarsLoader
+          v-if="loaderType === 'Bars'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxCirclesLoader
+          v-if="loaderType === 'Circles'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxGridLoader
+          v-if="loaderType === 'Grid'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxHeartsLoader
+          v-if="loaderType === 'Heart'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxOvalLoader
+          v-if="loaderType === 'Oval'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxPuffLoader
+          v-if="loaderType === 'Puff'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxRingsLoader
+          v-if="loaderType === 'Rings'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxSpinningCirclesLoader
+          v-if="loaderType === 'Spinning Circles'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxTailSpinLoader
+          v-if="loaderType === 'Tail Spin'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        />
+        <NxThreeDotsLoader
+          v-if="loaderType === 'Three Dots'"
+          v-for="(size, sizeIndex) in sizes"
+          :key="sizeIndex"
+          :isLoading="true"
+          :size="size"
+        /> -->
       </div>
     </section>
   </main>
 </template>
 
-<style scoped lang="scss">
+<script>
+import {
+  NxAudioLoader,
+  // NxBallTriangleLoader,
+  // NxBarsLoader,
+  // NxCirclesLoader,
+  // NxGridLoader,
+  // NxHeartsLoader,
+  // NxOvalLoader,
+  // NxPuffLoader,
+  // NxRingsLoader,
+  // NxSpinningCirclesLoader,
+  // NxTailSpinLoader,
+  // NxThreeDotsLoader,
+} from '@ngeenx/nx-vue-svg-loaders';
+
+export default {
+  components: {
+    NxAudioLoader,
+    // NxBallTriangleLoader,
+    // NxBarsLoader,
+    // NxCirclesLoader,
+    // NxGridLoader,
+    // NxHeartsLoader,
+    // NxOvalLoader,
+    // NxPuffLoader,
+    // NxRingsLoader,
+    // NxSpinningCirclesLoader,
+    // NxTailSpinLoader,
+    // NxThreeDotsLoader,
+  },
+  data() {
+    return {
+      sizes: [10, 15, 20, 25, 30, 25, 40, 50],
+      loaderTypes: [
+        'Audio',
+        'Ball Triangle',
+        'Bars',
+        'Circles',
+        'Grid',
+        'Heart',
+        'Oval',
+        'Puff',
+        'Rings',
+        'Spinning Circles',
+        'Tail Spin',
+        'Three Dots',
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
 @import './app.scss';
 </style>
---
+++ libs/features/nx-vue-svg-loaders/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./comonents/base-loader/base-loader";
+
+import NxAudioLoader from "./comonents/audio-loader/NxAudioLoader.vue";
+export { NxAudioLoader };
