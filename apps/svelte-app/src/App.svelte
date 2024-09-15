206ac26d374ceb6e405c1a20b3761e9c0f1bd039
relliv <epsilonelittr@gmail.com>
2024-09-15 06:18:15 +0300

    Add circles SVG loader

--- apps/svelte-app/src/App.svelte
+++ apps/svelte-app/src/App.svelte
@@ -3,6 +3,7 @@
     NxAudioLoader,
     NxBallTriangleLoader,
     NxBarsLoader,
+    NxCirclesLoader,
   } from '@ngeenx/nx-svelte-svg-loaders';
 
   const sizes = [10, 15, 20, 25, 30, 25, 40, 50],
@@ -41,6 +42,10 @@
 
           {#if loaderType === 'Bars'}
             <NxBarsLoader isLoading={true} {size} />
+          {/if}
+
+          {#if loaderType === 'Circles'}
+            <NxCirclesLoader isLoading={true} {size} />
           {/if}
         {/each}
       </div>
--- libs/features/nx-svelte-svg-loaders/src/index.ts
+++ libs/features/nx-svelte-svg-loaders/src/index.ts
@@ -3,5 +3,6 @@ export * from "./lib/components/base-loader/base-loader";
 import NxAudioLoader from "./lib/components/audio-loader/NxAudioLoader.svelte";
 import NxBallTriangleLoader from "./lib/components/ball-triangle-loader/NxBallTriangleLoader.svelte";
 import NxBarsLoader from "./lib/components/bars-loader/NxBarsLoader.svelte";
+import NxCirclesLoader from "./lib/components/circles-loader/NxCirclesLoader.svelte";
 
-export { NxAudioLoader, NxBallTriangleLoader, NxBarsLoader };
+export { NxAudioLoader, NxBallTriangleLoader, NxBarsLoader, NxCirclesLoader };
