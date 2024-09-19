c8e7de097f6b88280cfd75e7ef4daaf29f1cba79
relliv <epsilonelittr@gmail.com>
2024-09-19 15:07:45 +0300

    Update base folder

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://ngeenx.github.io/nx-svg-loaders/',
+  baseUrl: '/nx-svg-loaders/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
