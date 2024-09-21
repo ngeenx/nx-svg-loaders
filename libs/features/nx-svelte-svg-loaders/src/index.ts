7a71ba45256587b118f5a3e1faa81af79fcf1a46
relliv <epsilonelittr@gmail.com>
2024-09-21 07:19:19 +0300

    Simplify exports

--- libs/features/nx-svelte-svg-loaders/src/index.ts
+++ libs/features/nx-svelte-svg-loaders/src/index.ts
@@ -1,29 +1,14 @@
 export * from "./lib/components/base-loader/base-loader";
 
-import NxAudioLoader from "./lib/components/audio-loader/NxAudioLoader.svelte";
-import NxBallTriangleLoader from "./lib/components/ball-triangle-loader/NxBallTriangleLoader.svelte";
-import NxBarsLoader from "./lib/components/bars-loader/NxBarsLoader.svelte";
-import NxCirclesLoader from "./lib/components/circles-loader/NxCirclesLoader.svelte";
-import NxGridLoader from "./lib/components/grid-loader/NxGridLoader.svelte";
-import NxHeartsLoader from "./lib/components/hearts-loader/NxHeartsLoader.svelte";
-import NxOvalLoader from "./lib/components/oval-loader/NxOvalLoader.svelte";
-import NxPuffLoader from "./lib/components/puff-loader/NxPuffLoader.svelte";
-import NxRingsLoader from "./lib/components/rings-loader/NxRingsLoader.svelte";
-import NxSpinningCirclesLoader from "./lib/components/spinning-circles-loader/NxSpinningCirclesLoader.svelte";
-import NxTailSpinLoader from "./lib/components/tail-spin-loader/NxTailSpinLoader.svelte";
-import NxThreeDotsLoader from "./lib/components/three-dots-loader/NxThreeDotsLoader.svelte";
-
-export {
-  NxAudioLoader,
-  NxBallTriangleLoader,
-  NxBarsLoader,
-  NxCirclesLoader,
-  NxGridLoader,
-  NxHeartsLoader,
-  NxOvalLoader,
-  NxPuffLoader,
-  NxRingsLoader,
-  NxSpinningCirclesLoader,
-  NxTailSpinLoader,
-  NxThreeDotsLoader,
-};
+export { default as NxAudioLoader } from "./lib/components/audio-loader/NxAudioLoader.svelte";
+export { default as NxBallTriangleLoader } from "./lib/components/ball-triangle-loader/NxBallTriangleLoader.svelte";
+export { default as NxBarsLoader } from "./lib/components/bars-loader/NxBarsLoader.svelte";
+export { default as NxCirclesLoader } from "./lib/components/circles-loader/NxCirclesLoader.svelte";
+export { default as NxGridLoader } from "./lib/components/grid-loader/NxGridLoader.svelte";
+export { default as NxHeartsLoader } from "./lib/components/hearts-loader/NxHeartsLoader.svelte";
+export { default as NxOvalLoader } from "./lib/components/oval-loader/NxOvalLoader.svelte";
+export { default as NxPuffLoader } from "./lib/components/puff-loader/NxPuffLoader.svelte";
+export { default as NxRingsLoader } from "./lib/components/rings-loader/NxRingsLoader.svelte";
+export { default as NxSpinningCirclesLoader } from "./lib/components/spinning-circles-loader/NxSpinningCirclesLoader.svelte";
+export { default as NxTailSpinLoader } from "./lib/components/tail-spin-loader/NxTailSpinLoader.svelte";
+export { default as NxThreeDotsLoader } from "./lib/components/three-dots-loader/NxThreeDotsLoader.svelte";
