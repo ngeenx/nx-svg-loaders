d5cbf326d3466eb1b275bb8f768e4e0a7b74dbfd
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-10-02 15:57:49 +0300

    Update docs title and desription

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -33,8 +33,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Angular, React, Svelte, Vue SVG loader collection."
     >
       <HomepageHeader />
       <main>
