6e469d7cd6a2b87a7b44e8d95b29e50c76d0b34e
relliv <epsilonelittr@gmail.com>
2024-10-27 19:27:32 +0300

    Fix typo

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -16,7 +16,7 @@ const FeatureList = [
   {
     title: 'Powered by:',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: <>Angular, React, Svelte and Vue support</>,
+    description: <>Angular, React, Svelte and Vue</>,
   },
 ];
 
